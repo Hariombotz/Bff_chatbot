284a851bfa2e33578ce1d81c4d74dd96353e3ccc
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2025-06-17 21:37:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-<PUT FULL CODE FROM ABOVE>
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+LOG_CHANNEL = os.getenv("LOG_CHANNEL_ID")  # int or str
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split()))
+BOT_NAME = os.getenv("BOT_NAME", "BestieBot")
